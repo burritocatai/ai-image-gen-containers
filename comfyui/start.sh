1a9751da6834f8f0db2a42842f4549cd421e1016
Dan Wiseman <dan@thewisemans.io>
2025-02-03 15:33:22 -0500

    [Message] Automate custom node requirements installation
    This update adds a loop to check for `requirements.txt` in custom node directories and automatically installs the listed packages. It ensures all dependencies are met before starting ComfyUI, improving setup automation and reliability.

--- comfyui/start.sh
+++ comfyui/start.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 set -e  # Exit the script if any statement returns a non-true return value
 
-echo "Starting comfyui"
 cd /comfyui/ComfyUI && \
-	source venv/bin/activate && \
+source venv/bin/activate
+
+echo "checking missing nodes requirements"
+
+for dir in /comfyui/ComfyUI/custom_nodes/*/; do
+    if [ -f "$dir/requirements.txt" ]; then
+        echo "Installing requirements in $dir"
+        pip3 install -r "$dir/requirements.txt"
+    fi
+done
+
+echo "Starting comfyui"
+
 	python3 main.py \
     		--listen 0.0.0.0 \
     		--port ${COMFY_UI_PORT:-8082}
